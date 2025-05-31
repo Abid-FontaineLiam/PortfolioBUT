95424626d2398add7b191e0c2d29e88fe7957bb9
Abid-FontaineLiam <liam.abid--fontaine@edu.univ-fcomte.fr>
2025-05-31 17:45:39 +0200

    Update index,projet,styles + add terminal

---
+++ script/term_init.js
@@ -0,0 +1,65 @@
+function startAsciiLoadingBar(asciiBar) {
+    const spinnerFrames = ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'];
+    let spinnerIndex = 0;
+    const totalDuration = 3000;
+    const intervalTime = 100;
+    let elapsed = 0;
+    const loadingInterval = setInterval(() => {
+        spinnerIndex = (spinnerIndex + 1) % spinnerFrames.length;
+        asciiBar.textContent = spinnerFrames[spinnerIndex];
+        elapsed += intervalTime;
+        if (elapsed >= totalDuration) {
+            clearInterval(loadingInterval);
+            asciiBar.textContent = '✔';
+        }
+    }, intervalTime);
+}
+function startTextLoadingAnimation(element, baseText) {
+    let count = 0;
+    element.textContent = baseText;
+    const interval = setInterval(() => {
+        count = (count + 1) % 4;
+        element.textContent = baseText + '.'.repeat(count);
+    }, 500);
+    return interval;
+}
+function clearAndExpandTerminalScene() {
+    const terminalScene = document.getElementById('terminalScene');
+    terminalScene.removeAttribute('style');
+    if (terminalScene) {
+        terminalScene.innerHTML = '';
+        terminalScene.style.position = 'fixed';
+        terminalScene.style.top = '50%';
+        terminalScene.style.left = '50%';
+        terminalScene.style.transform = 'translate(-50%, -50%)';
+        terminalScene.style.width = '99%';
+        terminalScene.style.height = '98%';
+        terminalScene.style.background = '#000';
+        terminalScene.style.border = "1px solid #00FF00";
+        terminalScene.style.margin = '0';
+        terminalScene.style.padding = '0';
+        terminalScene.style.overflow = 'hidden';
+        terminalScene.style.display = 'flex';
+        terminalScene.style.alignItems = 'center';
+        terminalScene.style.justifyContent = 'center';        
+    }
+}
+const textElement = document.createElement('p');
+const placeholder = document.getElementById('init_placeholder');
+startTextLoadingAnimation(textElement,"Bienvenu");
+if (placeholder) {
+    placeholder.appendChild(textElement);
+    const asciiBar = document.createElement('pre');
+    asciiBar.style.fontFamily = 'IBMPlexMono';
+    asciiBar.style.marginTop = '10px';
+    asciiBar.style.fontSize = '18px';
+    placeholder.appendChild(asciiBar);
+    startAsciiLoadingBar(asciiBar);
+    setTimeout(() => {
+        clearAndExpandTerminalScene();
+        const script = document.createElement('script');
+        script.src = '/script/term_base.js';
+        script.type = 'text/javascript';
+        document.body.appendChild(script);
+    }, 4000);
+}
